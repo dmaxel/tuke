c07135c9b956ac65db0208bfa4b25c0094701bb0
Danny Stieben <dmaxel94@gmail.com>
2020-10-31 11:57:21 +0100

    Initial commit

---
+++ src/main/java/de/stieben/tuke/TukeApplication.java
@@ -0,0 +1,13 @@
+package de.stieben.tuke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TukeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TukeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/stieben/tuke/TukeApplicationTests.java
@@ -0,0 +1,13 @@
+package de.stieben.tuke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TukeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
